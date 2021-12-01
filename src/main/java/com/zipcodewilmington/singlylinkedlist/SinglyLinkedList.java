ae7d1345176446d78fac92558f3c3fc645604a04
Ellis John <ellisjohn@zipcodembpchuck.myfiosgateway.com>
2021-11-30 23:12:53 -0500

    lab

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -6,139 +6,250 @@ import java.util.LinkedList;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
 
-    class Node {
-        Object number;
-        Node next;
+    private Node<T> head;
+    private Node<T> tail;
 
-        public Node(Object number) {
-            this.number = number;
-            next = null;
+    private class Node<T> {
+
+        private T data;
+        private Node<T> next;
+
+        public Node() {
+
+        }
+
+        public Node(T data, Node<T> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public Node(T data) {
+            this.data = data;
         }
     }
 
+//        Object number;
+//        Node next;
+//
+//        public Node(Object number) {
+//            this.number = number;
+//            next = null;
+//        }
+//    }
+//
+//
+//    public Node head;
+//    public Node tail;
 
-    public Node head;
-    public Node tail;
+    public void add(T data) {
 
-    public void add(Object number) {
-        Node newNode = new Node(number);
-        if (this.head == null) {
-            this.head = newNode;
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
         } else {
             tail.next = newNode;
+            tail = newNode;
         }
-        tail = newNode;
+
+
+//        Node newNode = new Node(number);
+//        if (this.head == null) {
+//            this.head = newNode;
+//        } else {
+//            tail.next = newNode;
+//        }
+//        tail = newNode;
     }
 
 
     public void remove(int index) {
-        Node current = this.head;
-        Node prev = null;
 
-        if (index == 0 && current != null) {
-            this.head = current.next;
+        // given index to remove
+        // iterate through singly linked list
+        Node<T> currentNode = head, previousNode = null;
+        int currentIndex = 0;
+        // check if the current node exists
+
+        while (currentNode != null && currentIndex != index) {
+            // keeping track of the prev node
+            previousNode = currentNode;
+            // continuing the iteration
+            currentNode = currentNode.next;
+            // iterate through the next node
+            currentIndex++;
         }
-
-        int counter = 0;
-
-        while (current != null) {
-            if (counter == index) {
-                prev.next = current.next;
-                break;
-            } else {
-                prev = current;
-                current = current.next;
-                counter++;
+        //remove the index
+        if (currentIndex == index) {
+            if (previousNode != null) {
+                // add the previous node to the node after
+                previousNode.next = currentNode.next;
+            }
+            // checking if the current node is the head
+            if (currentNode == head) {
+                // and also if the current node is null
+                if (currentNode == null || currentNode.next == null) {
+                    // return head as null;
+                    head = null;
+                } else {
+                    // the head is the current head
+                    head = currentNode.next;
+                }
+            }
+            // current is the tail
+            else if (currentNode == tail) {
+                // then adjust the tail to the previous node
+                tail = previousNode;
             }
         }
-        if (current == null) {
-            System.out.println("Was not found.");
-        }
+
+
+//        Node current = this.head;
+//        Node prev = null;
+//
+//        if (index == 0 && current != null) {
+//            this.head = current.next;
+//        }
+//
+//        int counter = 0;
+//
+//        while (current != null) {
+//            if (counter == index) {
+//                prev.next = current.next;
+//                break;
+//            } else {
+//                prev = current;
+//                current = current.next;
+//                counter++;
+//            }
+//        }
+//        if (current == null) {
+//            System.out.println("Was not found.");
+//        }
     }
 
 
-    public boolean contains(Object key) {
-        Node current = this.head;
-        while (current != null) {
-            if (current.number != key) {
-                current = current.next;
+    public boolean contains(T element) {
+
+
+        Node<T> currentNode = head, previousNode = null;
+//        int currentIndex = 0;
+
+        while (currentNode != null) {
+            if (currentNode.data != element) {
+                currentNode = currentNode.next;
             } else {
                 return true;
             }
         }
         return false;
+
+
+//        Node current = this.head;
+//        while (current != null) {
+//            if (current.number != key) {
+//                current = current.next;
+//            } else {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//
+//    public Integer size() {
+//        Integer counter = 0;
+//        Node current = head;
+//
+//        while (current != null) {
+//            counter++;
+//            current = current.next;
+//        }
+//        return counter;
     }
 
-    public Integer size() {
-        Integer counter = 0;
-        Node current = head;
 
-        while (current != null) {
-            counter++;
-            current = current.next;
-        }
-        return counter;
-    }
+    public Integer find(T element) {
 
 
-    public Integer find(int data) {
-        Node current = this.head;
-        int index = 0;
+        int currentIndex = 0;
+        Node<T> currentNode = head;
 
-        while (current != null) {
-            if (!current.number.equals(data)) {
-                index++;
-                current = current.next;
-            } else {
-                return index;
+        while (currentNode != null) {
+            if (currentNode.data != element) {
+                currentNode = currentNode.next;
+                currentIndex++;
+            }
+            if (currentNode.data == element) {
+                return currentIndex;
             }
         }
         return -1;
+
+
+//        Node current = this.head;
+//        int index = 0;
+//
+//        while (current != null) {
+//            if (!current.number.equals(data)) {
+//                index++;
+//                current = current.next;
+//            } else {
+//                return index;
+//            }
+//        }
+//        return -1;
     }
 
 
     public Object get(int index) {
-        Node current = this.head;
-        int seek = 0;
-        while (current != null) {
-            if (seek == index) {
-                return current.number;
-            }
-            seek++;
-            current = current.next;
-        }
+
+
+//        Node current = this.head;
+//        int seek = 0;
+//        while (current != null) {
+//            if (seek == index) {
+//                return current.number;
+//            }
+//            seek++;
+//            current = current.next;
+//        }
         return -1;
     }
 
 
     public SinglyLinkedList copy() {
-        SinglyLinkedList copied = new SinglyLinkedList();
-        Node current = this.head;
-        while( current != null) {
-            copied.add(current.number);
-            current = current.next;
-        }
-        return copied;
-    }
+
+
+//        SinglyLinkedList copied = new SinglyLinkedList();
+//        Node current = this.head;
+//        while( current != null) {
+//            copied.add(current.number);
+//            current = current.next;
+//        }
+//        return SinglyLinkedList;
+//    }
 
 
     public void sort() {
-        Node current = this.head;
 
-        for (int i = 0; i < size(); i++) {
-            while (current.next != null) {
-                Node next = current.next;
-                if ((Integer) current.number <= (Integer) next.number) {
-                } else {
-                    Object temp = current.number;
-                    current.number = next.number;
-                    next.number = temp;
-                }
-                current = current.next;
-            }
-        }
+
+//        Node current = this.head;
+//
+//        for (int i = 0; i < size(); i++) {
+//            while (current.next != null) {
+//                Node next = current.next;
+//                if ((Integer) current.number <= (Integer) next.number) {
+//                } else {
+//                    Object temp = current.number;
+//                    current.number = next.number;
+//                    next.number = temp;
+//                }
+//                current = current.next;
+//            }
+//        }
 
     }
 //    public static void main(String[] args) {
